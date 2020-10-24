8651faf2285d2d6899c9d95d7b53b9d38a5b84c8
Charles NDAYISABA <nccharles1@gmail.com>
2020-10-24 17:17:08 +0200

    display default quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class QuoteComponent implements OnInit {
 
   Quotes = [
-    new Quote(1, 'Charles', '"Success is doing what you want to do, when you want, where you want, with whom you want, as much as you want."', new Date(2018, 8, 15) ),
-    new Quote(2, 'Kelly', '“It is not the critic who counts; not the man who points out how the strong man stumbles, or where the doer of deeds could have done them better. The credit belongs to the man who is actually in the arena, whose face is marred by dust and sweat and blood; who strives valiantly; who errs, who comes short again and again, because there is no effort without error and shortcoming; but who does actually strive to do the deeds; who knows great enthusiasms, the great devotions; who spends himself in a worthy cause; who at the best knows in the end the triumph of high achievement, and who at the worst, if he fails, at least fails while daring greatly, so that his place shall never be with those cold and timid souls who neither know victory nor defeat.”', new Date(2018, 8, 15) ),
+    new Quote(1, 'Jacqui Katona', 'People are going to have to take responsibility into their own hands. We can’t leave it in the hands of politicians and petty bureaucrats. All power and strength to the people. I reckon we’ll do it. Absolutely!', new Date(2020, 10, 15) ),
+    new Quote(2, 'Robyn Davidsony', 'The two important things that I did learn were that you are as powerful and strong as you allow yourself to be, and that the most difficult part of any endeavour is taking the first step, making the first decision.', new Date(2020, 10, 12) ),
   ]
 
   constructor() { }
