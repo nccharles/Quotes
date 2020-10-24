46059768b1a4f03817a24deed7fcbf547326bbc0
Charles NDAYISABA <nccharles1@gmail.com>
2020-10-24 18:21:49 +0200

    added pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { StrikethroughDirective } from './strikethrough.directive';
     QuoteComponent,
     QuoteDetailsComponent,
     StrikethroughDirective,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
