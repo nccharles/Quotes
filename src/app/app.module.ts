ca01e3cb90d3e6514157f923e27b281ed9afc6e2
Charles NDAYISABA <nccharles1@gmail.com>
2020-10-24 16:56:02 +0200

    details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,16 @@
+import { Quote } from './../quote';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  Quotes = [
+    new Quote(1, 'Charles', '"Success is doing what you want to do, when you want, where you want, with whom you want, as much as you want."', new Date(2018, 8, 15) ),
+    new Quote(2, 'Kelly', '“It is not the critic who counts; not the man who points out how the strong man stumbles, or where the doer of deeds could have done them better. The credit belongs to the man who is actually in the arena, whose face is marred by dust and sweat and blood; who strives valiantly; who errs, who comes short again and again, because there is no effort without error and shortcoming; but who does actually strive to do the deeds; who knows great enthusiasms, the great devotions; who spends himself in a worthy cause; who at the best knows in the end the triumph of high achievement, and who at the worst, if he fails, at least fails while daring greatly, so that his place shall never be with those cold and timid souls who neither know victory nor defeat.”', new Date(2018, 8, 15) ),
+  ]
 
   constructor() { }
 
